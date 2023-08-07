9feb8a423aea2c385eec218a4c3d580f16491c25
dmitrisg <100453615+dmitrisg@users.noreply.github.com>
2023-08-07 15:23:19 -0500

    added existing files

---
+++ main.js
@@ -0,0 +1,8 @@
+function alterText(){
+	let text = document.getElementById("comment");
+	text.style["color"] = "yellow";
+	text.style["font-size"] = "40px";
+	text.style["text-transform"] = "capitalize";
+	text.style["text-decoration-line"] = "underline";           
+}
+
